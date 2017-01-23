9db9d92f0c725b39968015c9e05d24904aa70d61
Thanh Phú <thanhphu@users.noreply.github.com>
2017-01-23 11:00:29 +0900

    Update mongo syntax to mongo 3

--- src/r-mongo-scala/src/main/scala/rmongo/RMongo.scala
+++ src/r-mongo-scala/src/main/scala/rmongo/RMongo.scala
@@ -1,205 +1,211 @@
 package rmongo
 
-import com.mongodb.util.JSON
-import collection.mutable.ListBuffer
-import collection.JavaConversions._
+import java.util
+import java.util.logging.{Level, Logger}
+
 import com.mongodb._
-import java.util.HashSet
-import java.util.Iterator
-import java.util.Arrays
-import java.util.logging.Logger
-import java.util.logging.Level
+import com.mongodb.util.JSON
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable.ListBuffer
 
 class RMongo(dbName: String, hosts: String, replica: Boolean, username: String, pwd: String) {
-  val mongoLogger = Logger.getLogger("com.mongodb")
-  mongoLogger.setLevel(Level.SEVERE)
-  val servers = hosts.split(",").map(_.trim.split(":")).map { a =>
-    if (a.size < 2) new ServerAddress(a(0), 27017) else new ServerAddress(a(0), a(1).toInt)
-  }.toList
-  val this.replica = replica
-  val credential = mCreateCredential(username,dbName,pwd)
-  val m = if (!this.replica) new MongoClient(servers(0)) else new MongoClient(servers,List(credential))
-  val db = m.getDB(dbName)
-  var writeConcern = WriteConcern.NORMAL
+	val mongoLogger = Logger.getLogger("com.mongodb")
+	mongoLogger.setLevel(Level.SEVERE)
+	val servers = hosts.split(",").map(_.trim.split(":")).map { a =>
+		if (a.length < 2) new ServerAddress(a(0), 27017) else new ServerAddress(a(0), a(1).toInt)
+	}.toList
+	val this.replica = replica
+	val credential = mCreateCredential(username, dbName, pwd)
+	val m = if (!this.replica || username.isEmpty || pwd.isEmpty) {
+		new MongoClient(servers.head)
+	} else {
+		new MongoClient(servers, List(credential))
+	}
+	val db = m.getDB(dbName)
+	// val newDB = m.getDatabase(dbName)
+	var writeConcern = WriteConcern.ACKNOWLEDGED
 
-  def this(dbName: String, host: String, port: Int) = this (dbName, host + ":" + port, false,"","")
+	def this(dbName: String, host: String, replica: Boolean) = this(dbName, host + ":27017", replica, "", "")
 
-  def this(dbName: String) = this (dbName, "127.0.0.1:27017", false,"","")
+	def this(dbName: String, host: String, port: Int) = this(dbName, host + ":" + port, false, "", "")
 
-  def dbAuthenticate(username:String, password:String):Boolean = {
-    db.authenticate(username, password.toCharArray)
-  }
-  
-  def mCreateCredential(userName:String, database:String,password:String):MongoCredential = {
-	MongoCredential.createCredential(username,database,password.toCharArray)
-  }
+	def this(dbName: String) = this(dbName, "127.0.0.1:27017", false, "", "")
 
-  def dbSetWriteConcern(w: Int, wtimeout: Int, fsync: Boolean, j: Boolean) {
-    writeConcern = new WriteConcern(w, wtimeout, fsync, j)
-  }
+	def mCreateCredential(userName: String, database: String, password: String): MongoCredential = {
+		MongoCredential.createCredential(username, database, password.toCharArray)
+	}
 
-  def dbShowCollections():Array[String] = {
-    val names = db.getCollectionNames().map(name => name)
+	def dbSetWriteConcern(w: Int, wtimeout: Int, fsync: Boolean, j: Boolean) {
+		writeConcern = new WriteConcern(w, wtimeout)
+	}
 
-    names.toArray
-  }
+	def dbShowCollections(): Array[String] = {
+		db.getCollectionNames.map(name => name).toArray
+	}
 
-  def dbInsertDocument(collectionName: String, jsonDoc: String): String = {
-    dbModificationAction(collectionName, jsonDoc, _.insert(_, writeConcern))
-  }
+	def dbInsertDocument(collectionName: String, jsonDoc: String): String = {
+		dbModificationAction(collectionName, jsonDoc, _.insert(_, writeConcern))
+	}
 
-  def dbRemoveQuery(collectionName: String, query: String): String = {
-    dbModificationAction(collectionName, query, _.remove(_, writeConcern))
-  }
+	def dbRemoveQuery(collectionName: String, query: String): String = {
+		dbModificationAction(collectionName, query, _.remove(_, writeConcern))
+	}
 
-  def dbModificationAction(collectionName: String,
-                          query: String,
-                          action:(DBCollection, DBObject) => WriteResult): String = {
-    val dbCollection = db.getCollection(collectionName)
+	def dbModificationAction(collectionName: String,
+	                         query: String,
+	                         action: (DBCollection, DBObject) => WriteResult): String = {
+		val dbCollection = db.getCollection(collectionName)
 
-    val queryObject = JSON.parse(query).asInstanceOf[DBObject]
-    val results = action(dbCollection, queryObject)
+		val queryObject = JSON.parse(query).asInstanceOf[DBObject]
+		try {
+			val results = action(dbCollection, queryObject)
 
-    if(results.getError == null) "ok" else results.getError
-  }
+			if (results != null) "ok" else "error"
+		} catch  {
+			case e: Exception => e.getMessage
+		}
+	}
 
-  def dbGetQuery(collectionName: String, query: String): String = {
-    dbGetQuery(collectionName, query, 0, 1000)
-  }
+	def dbGetQuery(collectionName: String, query: String): String = {
+		// newDB.getCollection(collectionName).find(BsonDocument.parse(query)).head.toJson
+		dbGetQuery(collectionName, query, 0, 1000)
+	}
 
-  def dbGetQuery(collectionName: String, query: String,
-                                  skipNum:Double, limitNum:Double):String = {
-    dbGetQuery(collectionName, query, skipNum.toInt, limitNum.toInt)
-  }
+	def dbGetQuery(collectionName: String, query: String,
+	               skipNum: Double, limitNum: Double): String = {
+		dbGetQuery(collectionName, query, skipNum.toInt, limitNum.toInt)
+	}
 
-  def dbGetQuery(collectionName: String, query: String,
-                         skipNum:Int, limitNum:Int): String = {
-    dbGetQuery(collectionName, query, "{}", skipNum, limitNum)
- }
+	def dbGetQuery(collectionName: String, query: String,
+	               skipNum: Int, limitNum: Int): String = {
+		dbGetQuery(collectionName, query, "{}", skipNum, limitNum)
+	}
 
-  def dbGetQuery(collectionName:String, query:String, keys:String): String = {
-    dbGetQuery(collectionName, query, keys, 0, 1000)
-  }
+	def dbGetQuery(collectionName: String, query: String, keys: String): String = {
+		dbGetQuery(collectionName, query, keys, 0, 1000)
+	}
 
-  def dbGetQuery(collectionName: String, query: String, keys:String,
-                                  skipNum:Double, limitNum:Double):String = {
-    dbGetQuery(collectionName, query, keys, skipNum.toInt, limitNum.toInt)
-  }
+	def dbGetQuery(collectionName: String, query: String, keys: String,
+	               skipNum: Double, limitNum: Double): String = {
+		dbGetQuery(collectionName, query, keys, skipNum.toInt, limitNum.toInt)
+	}
 
-  def dbGetQuery(collectionName: String, query: String, keys: String,
-                         skipNum:Int, limitNum:Int): String = {
-    val dbCollection = db.getCollection(collectionName)
+	def dbGetQuery(collectionName: String, query: String, keys: String,
+	               skipNum: Int, limitNum: Int): String = {
+		val dbCollection = db.getCollection(collectionName)
 
-    val queryObject = JSON.parse(query).asInstanceOf[DBObject]
-    val keysObject = JSON.parse(keys).asInstanceOf[DBObject]
-    val cursor = dbCollection.find(queryObject, keysObject).skip(skipNum).limit(limitNum)
+		val queryObject = JSON.parse(query).asInstanceOf[DBObject]
+		val keysObject = JSON.parse(keys).asInstanceOf[DBObject]
+		val cursor = dbCollection.find(queryObject, keysObject).skip(skipNum).limit(limitNum)
 
-    val results = RMongo.toCsvOutput(cursor)
+		val results = RMongo.toCsvOutput(cursor)
 
-    results
-  }
+		results
+	}
 
-  def dbGetDistinct(collectionName: String, key: String): String = {
-    dbGetDistinct(collectionName, key, "")
-  }
+	def dbGetDistinct(collectionName: String, key: String): String = {
+		dbGetDistinct(collectionName, key, "")
+	}
 
-  def dbGetDistinct(collectionName: String, key: String, query: String): String = {
-    val dbCollection = db.getCollection(collectionName)
+	def dbGetDistinct(collectionName: String, key: String, query: String): String = {
+		val dbCollection = db.getCollection(collectionName)
 
-    val queryObject = JSON.parse(query).asInstanceOf[DBObject]
-    val distinctResults = dbCollection.distinct(key, queryObject).iterator
-    val results = ListBuffer[String]()
+		val queryObject = JSON.parse(query).asInstanceOf[DBObject]
+		val distinctResults = dbCollection.distinct(key, queryObject).iterator
+		val results = ListBuffer[String]()
 
-    while (distinctResults.hasNext) {
-      val item = distinctResults.next
-      results.append("\"" + item.toString.replaceAll("\n", "\\n") + "\"")
-    }
+		while (distinctResults.hasNext) {
+			val item = distinctResults.next
+			results.append("\"" + item.toString.replaceAll("\n", "\\n") + "\"")
+		}
 
-    results.mkString("\n")
-  }
+		results.mkString("\n")
+	}
 
-  //def dbAggregate(collectionName: String, queries: Array[String]): String = {
-  def dbAggregate(collectionName: String, queries: Array[String]):Array[String] = {
-    val dbCollection = db.getCollection(collectionName)
-    val queryArray = new Array[DBObject](queries.length)
-    for ( i <- 0 to (queries.length - 1) ) {
-        val query = queries(i)
-        queryArray(i) = JSON.parse(query).asInstanceOf[DBObject]
-    }
-    var aggregateIterator = dbCollection.aggregate(queryArray(0), Arrays.copyOfRange(queryArray, 1, queryArray.length):_*).results.iterator
+	//def dbAggregate(collectionName: String, queries: Array[String]): String = {
+	def dbAggregate(collectionName: String, queries: Array[String]): Array[String] = {
+		val dbCollection = db.getCollection(collectionName)
+		val queryArray = new Array[DBObject](queries.length)
+		for (i <- queries.indices) {
+			val query = queries(i)
+			queryArray(i) = JSON.parse(query).asInstanceOf[DBObject]
+		}
+		val aggregateIterator = dbCollection.aggregate(queryArray(0), util.Arrays.copyOfRange(queryArray, 1, queryArray.length): _*).results.iterator
 
-    val results = ListBuffer[String]()
-    while (aggregateIterator.hasNext) {
-      val item = aggregateIterator.next
-      // results.append("\"" + item.toString.replaceAll("\n", "\\n") + "\"")
-      results.append(item.toString)
-    }
-    //results.mkString("\n")
-    results.toArray
-  }
+		val results = ListBuffer[String]()
+		while (aggregateIterator.hasNext) {
+			val item = aggregateIterator.next
+			// results.append("\"" + item.toString.replaceAll("\n", "\\n") + "\"")
+			results.append(item.toString)
+		}
+		//results.mkString("\n")
+		results.toArray
+	}
 
-  def close() {
-    m.close()
-  }
+	def close() {
+		m.close()
+	}
 
-  def main() {
+	def main() {
 
-  }
+	}
 }
 
-object RMongo{
-  val SEPARATOR = ""
+object RMongo {
+	val SEPARATOR = ""
 
-  def toJsonOutput(cursor:DBCursor): String = {
-    val results = ListBuffer[String]()
-    while (cursor.hasNext) {
-      val item = cursor.next
-      results.append(item.toString)
-    }
+	def toJsonOutput(cursor: DBCursor): String = {
+		val results = ListBuffer[String]()
+		while (cursor.hasNext) {
+			val item = cursor.next
+			results.append(item.toString)
+		}
 
-    results.mkString("[", SEPARATOR, "]")
-  }
+		results.mkString("[", SEPARATOR, "]")
+	}
 
-  def toCsvOutput(cursor: DBCursor): String = {
+	def toCsvOutput(cursor: DBCursor): String = {
 
-    if(cursor.hasNext == false) return ""
+		if (!cursor.hasNext) return ""
 
-    val results = ListBuffer[String]()
+		val results = ListBuffer[String]()
 
-    if (cursor.getKeysWanted != null && cursor.getKeysWanted.keySet.size != 0) {
-        /* If fields were specified (save time) */
-        val keysWanted = cursor.getKeysWanted
-        keysWanted.put("_id", 1)
-        val keys = keysWanted.keySet.toArray(new Array[String](keysWanted.keySet.size))
-        results.append(keys.mkString(SEPARATOR))
-        while (cursor.hasNext) {
-            results.append(csvRowFromDBObject(keys, cursor.next))
-        }
-    } else {
-        /* Try to find set of desired keys by scanning cursor */
-        val keysWanted = new HashSet[String]()
-        val ccursor:DBCursor = cursor.copy
-        while(ccursor.hasNext == true) {
-            keysWanted.addAll(ccursor.next.keySet)
-        }
-        val keys = keysWanted.toArray(new Array[String](keysWanted.size))
-        results.append(keys.mkString(SEPARATOR))
-        while (cursor.hasNext) {
-            results.append(csvRowFromDBObject(keys, cursor.next))
-        }
-    }
+		if (cursor.getKeysWanted != null && cursor.getKeysWanted.keySet.size != 0) {
+			/* If fields were specified (save time) */
+			val keysWanted = cursor.getKeysWanted
+			keysWanted.put("_id", 1)
+			val keys = keysWanted.keySet.toArray(new Array[String](keysWanted.keySet.size))
+			results.append(keys.mkString(SEPARATOR))
+			while (cursor.hasNext) {
+				results.append(csvRowFromDBObject(keys, cursor.next))
+			}
+		} else {
+			/* Try to find set of desired keys by scanning cursor */
+			val keysWanted = new util.HashSet[String]()
+			val ccursor: DBCursor = cursor.copy
+			while (ccursor.hasNext) {
+				keysWanted.addAll(ccursor.next.keySet)
+			}
+			val keys = keysWanted.toArray(new Array[String](keysWanted.size))
+			results.append(keys.mkString(SEPARATOR))
+			while (cursor.hasNext) {
+				results.append(csvRowFromDBObject(keys, cursor.next))
+			}
+		}
 
-    results.mkString("\n")
-  }
+		results.mkString("\n")
+	}
 
-  def csvRowFromDBObject(keys:Array[String], item:DBObject):String = {
+	def csvRowFromDBObject(keys: Array[String], item: DBObject): String = {
 
-    keys.map{k =>
-      val value = item.get(k)
+		keys.map { k =>
+			val value = item.get(k)
 
-      if(value != null)
-        value.toString.replaceAll("\"", "\\\"")
-      else
-        "" }.mkString(SEPARATOR)
-  }
+			if (value != null)
+				value.toString.replaceAll("\"", "\\\"")
+			else
+				""
+		}.mkString(SEPARATOR)
+	}
 }
